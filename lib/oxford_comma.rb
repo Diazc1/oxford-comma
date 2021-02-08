2492e17c0d5bec1a21c583411fbbf141618e4881
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-08 14:41:04 -0800

    commit tree f817c46807384824fcaecedb6defbe3d01fbc8e9

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
-
+def oxford_comma(array) #method oxford_comma takes an arguement "array" of string elements and converts it into a string using the oxford comma
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(" and ")
+    else
+        lastStr = array.pop
+        array.join(", ") << ", and #{lastStr}"
+    end
 end
